bc21ba063aee8fef347c5b02c16e0396406d9247
Anthony Colle <anthonycolle84@gmail.com>
2019-05-17 09:42:11 +0100

    Fixed decimals in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             //console.log(body.daily.data[0])
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + Math.floor(body.currently.temperature) + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
